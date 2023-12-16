1bab22af4d5124c9c628252fd79ddbcef3e244de
sandeshdevkota <96155382+sandeshdevkota@users.noreply.github.com>
2023-12-16 18:10:12 +0545

    Delete js directory

--- js/animation.js
+++
@@ -1,9 +0,0 @@
-// Add JavaScript to toggle visibility class after a delay -->
-
-    // Add JavaScript to toggle visibility class after a delay
-    document.addEventListener('DOMContentLoaded', function () {
-      var startAfterDelayElement = document.getElementById('startAfterDelay');
-      setTimeout(function () {
-        startAfterDelayElement.classList.remove('hide-animation');
-      }, 3200); // 3.2s delay before displaying
-    });
